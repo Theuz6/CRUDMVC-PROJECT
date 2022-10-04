9efe9305e843576378e250fa6a668dee07a1a988
theuz6 <msantossilva005@gmail.com>
2022-10-03 22:58:42 -0300

    Primeiro commit

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CRUDMVC.Models;
+
+namespace CRUDMVC.Controllers;
+
+public class ContatoController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Criar()
+    {
+        return View();
+    }
+    public IActionResult Editar()
+    {
+        return View();
+    }
+    public IActionResult VerificarDeletar()
+    {
+        return View();
+    }
+}
