7dbad40fc3540bb882831246ca483a6dfb3ac721
Matheus Silva <msantossilva005@gmail.com>
2022-10-20 13:51:44 -0300

    update

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace CRUDMVC_PROJECT.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
